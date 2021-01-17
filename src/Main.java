04ac53b2b83ed9a2968ce3b005ce1ca792228178
shonel <srahim457@gmail.com>
2021-01-17 03:59:14 -0500

    Added some graphing capability

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import javax.swing.*;
 import java.util.concurrent.TimeUnit;
+import java.util.*;
+
 
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //creating a household and populating with farmers
         Household test = new Household(1, 100, 5, 7, 2);
         Farmer a = new Farmer(10);
@@ -13,13 +16,17 @@ public class Main {
 
         //loop for 1 year (52 weeks)
         System.out.print("Food Stores");
+        System.out.println();
+        //going to label the week, so initialize the string here
+        String weekName = "";
+        Vector<Integer> foodStoreList = new Vector<Integer>(53);
         for(int i = 0; i <= 52; i++){
-            //ghetto display bar, didn't want to introduce dependency this early
-            System.out.print(i);
-            System.out.print(" week: ");
+            //ghetto display table, cuz f***
+            weekName = " week" + i + ' ';
+            System.out.print(weekName);
             System.out.print(test.foodStores);
+            foodStoreList.add(test.foodStores);
             System.out.println();
-
             /*
             try {
                 TimeUnit.SECONDS.sleep(1); //so that you can see display in real time
@@ -39,8 +46,12 @@ public class Main {
             for(Farmer temp : test.farmerList){
                 temp.renewLabor();
             }
-
-
         }
+        //generates a bar graph object, and the following functions are display parameters
+        DisplayFoodStores_Bar example = new DisplayFoodStores_Bar("plot of food stored", foodStoreList);
+        example.setSize( 1200, 400);
+        example.setLocationRelativeTo(null);
+        example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        example.setVisible(true);
     }
 }
