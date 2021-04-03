b9a7b5bc37e0923b807061462a3d569c6f78afdb
Fred Kozlowski <kozlowskifrederick@gmail.com>
2021-04-03 18:35:10 -0400

    Added graphing for merchant

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
-
         Village village1 = new Village(1,4);
         Village village2 = new Village(2,5);
         ArrayList<Village> villageList = new ArrayList<>();
@@ -14,21 +12,21 @@ public class Main {
 
         int x = 30;
         int y = 30;
-        /*for(Household h : village1.householdList){
-            x += 70;
-            y += 70;
-            h.graphFood(x, y);
-        }
-        for(Household h : village2.householdList){
-            x += 70;
-            y += 70;
-            h.graphFood(x, y);
-        }*/
+
         village1.graphFood(x, y);
         village2.graphFood(x + 30, y + 30);
-        //to do refactor graphing to be separate class outside of household, to be used on manor as well, if possible
 
         Merchant merchant = new Merchant(111);
-        merchant.trade(village1.manor, Crops.Barley);
+        for(int i = 0; i <= 52; i++) {
+            if(i == 30){
+                merchant.trade(village1.manor, Crops.Barley);
+            }
+            if(i == 40){
+                merchant.trade(village2.manor, Crops.Barley);
+            }
+            merchant.updateWallet();
+        }
+        merchant.graph(x + 60, y + 60);
+
     }
 }
--- src/Merchant.java
+++ src/Merchant.java
@@ -1,3 +1,6 @@
+import Graphing.DisplayMerchantGraph;
+
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class Merchant extends Person{
@@ -5,12 +8,14 @@ public class Merchant extends Person{
     ArrayList<Integer> inventory;
     int location; //id of current location
     int wallet; //What's in your wallet?
+    ArrayList<Integer> walletDataset;
 
     public Merchant(int id){
         super(id);
         this.inventory = new ArrayList<>();
         this.location = 1; //where the merchant spawns
         this.wallet = 100; //need to decide what currency to use
+        this.walletDataset = new ArrayList<>();
     }
 
     public int trade(Manor tradingWith, Crops object){ //probably extend later past just crops
@@ -29,5 +34,18 @@ public class Merchant extends Person{
         System.out.println("My Merchant License No.:"+this.id);
         System.out.println("Wallet:" + this.wallet);
         System.out.println("Knapsack:"+ this.inventory);
+    }
+
+    public void updateWallet(){
+        walletDataset.add(wallet);
+    }
+
+    public void graph(int x, int y){
+        //generates a bar graph object, and the following functions are display parameters
+        DisplayMerchantGraph graph = new DisplayMerchantGraph("plot of merchant wallet", walletDataset);
+        graph.setSize( 600, 400);
+        graph.setLocation(x, y);
+        graph.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        graph.setVisible(true);
     }
 }
